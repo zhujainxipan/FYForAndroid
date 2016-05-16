3915433138e986a8f29f1a94b0b2b14c5eb59c5d
安诺爱成长 <1399487511@qq.com>
2016-05-16 21:32:48 +0800

    add BaseActivity&BaseFragment

---
+++ app/src/main/java/com/ht/fyforandroid/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.ht.fyforandroid;
+
+import android.app.Activity;
+
+/**
+ * Created by niehongtao on 16/5/16.
+ */
+public class BaseActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/ht/fyforandroid/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.ht.fyforandroid;
+
+import android.app.Fragment;
+
+/**
+ * Created by niehongtao on 16/5/16.
+ */
+public class BaseFragment extends Fragment {
+}
