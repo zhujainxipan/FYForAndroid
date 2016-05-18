8f194ea3219ea8ebddde571f098314a8c883aa69
安诺爱成长 <1399487511@qq.com>
2016-05-18 19:38:55 +0800

    引入v4包

---
+++ app/src/main/java/com/ht/fyforandroid/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.ht.fyforandroid.base;
+
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by niehongtao on 16/5/16.
+ */
+public class BaseActivity extends FragmentActivity {
+
+}
