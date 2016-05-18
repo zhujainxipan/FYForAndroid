bd6e658f222432fb69bd5e7ece96499c27cb42ab
安诺爱成长 <1399487511@qq.com>
2016-05-18 20:29:21 +0800

    引入events

--- app/src/main/java/com/ht/fyforandroid/base/BaseFragment.java
+++ app/src/main/java/com/ht/fyforandroid/base/BaseFragment.java
@@ -1,9 +1,62 @@
 package com.ht.fyforandroid.base;
 
-import android.app.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+import de.greenrobot.event.EventBus;
 
 /**
  * Created by niehongtao on 16/5/16.
  */
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutId(), container, false);
+        return view;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.inject(this, view);
+        initView(view);
+        initData();
+        if (isOpenEventBus()) {
+            openEventBus();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+    }
+
+    private void openEventBus() {
+        EventBus.getDefault().register(this);
+    }
+
+    /**
+     * 设置fragment的布局
+     * @return
+     */
+    protected abstract int getLayoutId();
+
+    protected abstract int initView(View view);
+
+    protected abstract void initData();
+
+    protected abstract boolean isOpenEventBus();
+
 }
