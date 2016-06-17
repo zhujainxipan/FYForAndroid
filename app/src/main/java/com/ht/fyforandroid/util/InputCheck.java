77fec9aceb75a1e95db4c2abf456bf3029258179
安诺爱成长 <1399487511@qq.com>
2016-06-17 15:19:23 +0800

    增加验证输入信息的工具类

---
+++ app/src/main/java/com/ht/fyforandroid/util/InputCheck.java
@@ -0,0 +1,43 @@
+package com.ht.fyforandroid.util;
+
+import android.content.Context;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by niehongtao on 16/6/17.
+ * 验证输入信息是否合理
+ */
+public class InputCheck {
+    public static boolean isEmail(String s) {
+        String regExp = "^.+@.+$";
+        Pattern p = Pattern.compile(regExp);
+        Matcher m = p.matcher(s);
+        return m.find();
+    }
+
+    public static boolean isPhone(String s) {
+        String regExp = "^[0-9]{11}$";
+        Pattern p = Pattern.compile(regExp);
+        Matcher m = p.matcher(s);
+        return m.find();
+    }
+
+    public static boolean checkEmail(Context context, String s) {
+        boolean result = isEmail(s);
+        if (!result) {
+            ToastUtil.shortShow("您输入的 Email 格式错误");
+        }
+
+        return result;
+    }
+
+    public static boolean checkPhone(Context context, String s) {
+        boolean result = isPhone(s);
+        if (!result) {
+            ToastUtil.shortShow("您输入的电话号码格式错误");
+        }
+        return result;
+    }
+}
--- app/src/main/java/com/ht/fyforandroid/util/TDevice.java
+++ app/src/main/java/com/ht/fyforandroid/util/TDevice.java
@@ -66,12 +66,19 @@ public class TDevice {
 
 
     /**
-     * dp转化为px
-     * @param dp
-     * @return
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
      */
-    public static float dpToPixel(float dp) {
-        return dp * (getDisplayMetrics().densityDpi / 160F);
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
     }
 
     /**
