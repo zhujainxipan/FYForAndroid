61f4a1db262ceab95b42dac6ed19517838ec0407
安诺爱成长 <1399487511@qq.com>
2016-07-07 14:28:02 +0800

    增加DensityUtils、

--- app/src/main/java/com/ht/fyforandroid/util/StringUtils.java
+++ app/src/main/java/com/ht/fyforandroid/util/StringUtils.java
@@ -1,28 +1,213 @@
+/*
+ * Copyright (c) 2014,KJFrameForAndroid Open Source Project,张涛.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ht.fyforandroid.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 import java.util.regex.Pattern;
 
 /**
- * Created by niehongtao on 16/5/20.
+ * 字符串操作工具包<br>
+ *
+ * <b>创建时间</b> 2014-8-14
+ *
+ * @author kymjs (https://github.com/kymjs)
+ * @version 1.1
  */
 public class StringUtils {
     private final static Pattern emailer = Pattern
             .compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
+    private final static Pattern phone = Pattern
+            .compile("^((13[0-9])|(15[^4,\\D])|(18[0,5-9]))\\d{8}$");
 
-    private final static Pattern IMG_URL = Pattern
-            .compile(".*?(gif|jpeg|png|jpg|bmp)");
+    /**
+     * 判断给定字符串是否空白串 空白串是指由空格、制表符、回车符、换行符组成的字符串 若输入字符串为null或空字符串，返回true
+     */
+    public static boolean isEmpty(CharSequence input) {
+        if (input == null || "".equals(input))
+            return true;
 
-    private final static Pattern URL = Pattern
-            .compile("^(https|http)://.*?$(net|com|.com.cn|org|me|)");
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 判断给定字符串是否空白串 空白串是指由空格、制表符、回车符、换行符组成的字符串 若输入字符串为null或空字符串，返回true
+     */
+    public static boolean isEmpty(CharSequence... strs) {
+        for (CharSequence str : strs) {
+            if (isEmpty(str)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断是不是一个合法的电子邮件地址
+     */
+    public static boolean isEmail(CharSequence email) {
+        if (isEmpty(email))
+            return false;
+        return emailer.matcher(email).matches();
+    }
+
+    /**
+     * 判断是不是一个合法的手机号码
+     */
+    public static boolean isPhone(CharSequence phoneNum) {
+        if (isEmpty(phoneNum))
+            return false;
+        return phone.matcher(phoneNum).matches();
+    }
+
+    /**
+     * 返回当前系统时间
+     */
+    public static String getDataTime(String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        return df.format(new Date());
+    }
+
+    /**
+     * 字符串转整数
+     *
+     * @param str
+     * @param defValue
+     * @return
+     */
+    public static int toInt(String str, int defValue) {
+        try {
+            return Integer.parseInt(str);
+        } catch (Exception e) {
+        }
+        return defValue;
+    }
+
+    /**
+     * 对象转整
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static int toInt(Object obj) {
+        if (obj == null)
+            return 0;
+        return toInt(obj.toString(), 0);
+    }
+
+    /**
+     * String转long
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static long toLong(String obj) {
+        try {
+            return Long.parseLong(obj);
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+
+    /**
+     * String转double
+     *
+     * @param obj
+     * @return 转换异常返回 0
+     */
+    public static double toDouble(String obj) {
+        try {
+            return Double.parseDouble(obj);
+        } catch (Exception e) {
+        }
+        return 0D;
+    }
+
+    /**
+     * 字符串转布尔
+     *
+     * @param b
+     * @return 转换异常返回 false
+     */
+    public static boolean toBool(String b) {
+        try {
+            return Boolean.parseBoolean(b);
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    /**
+     * 判断一个字符串是不是数字
+     */
+    public static boolean isNumber(CharSequence str) {
+        try {
+            Integer.parseInt(str.toString());
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * byte[]数组转换为16进制的字符串。
+     *
+     * @param data
+     *            要转换的字节数组。
+     * @return 转换后的结果。
+     */
+    public static final String byteArrayToHexString(byte[] data) {
+        StringBuilder sb = new StringBuilder(data.length * 2);
+        for (byte b : data) {
+            int v = b & 0xff;
+            if (v < 16) {
+                sb.append('0');
+            }
+            sb.append(Integer.toHexString(v));
+        }
+        return sb.toString().toUpperCase(Locale.getDefault());
+    }
+
+    /**
+     * 16进制表示的字符串转换为字节数组。
+     *
+     * @param s
+     *            16进制表示的字符串
+     * @return byte[] 字节数组
+     */
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] d = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            // 两位一组，表示一个字节,把这样表示的16进制字符串，还原成一个进制字节
+            d[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
+                    .digit(s.charAt(i + 1), 16));
+        }
+        return d;
+    }
 
     private final static ThreadLocal<SimpleDateFormat> dateFormater = new ThreadLocal<SimpleDateFormat>() {
         @Override
@@ -39,41 +224,20 @@ public class StringUtils {
     };
 
     /**
-     * 将字符串转位日期类型
-     *
-     * @param sdate
-     * @return
-     */
-    public static Date toDate(String sdate) {
-        return toDate(sdate, dateFormater.get());
-    }
-
-    public static Date toDate(String sdate, SimpleDateFormat dateFormater) {
-        try {
-            return dateFormater.parse(sdate);
-        } catch (ParseException e) {
-            return null;
-        }
-    }
-
-    public static String getDateString(Date date) {
-        return dateFormater.get().format(date);
-    }
-
-    /**
      * 以友好的方式显示时间
      *
      * @param sdate
      * @return
      */
-    public static String friendly_time(String sdate) {
+    public static String friendlyTime(String sdate) {
         Date time = null;
 
-        if (TimeZoneUtil.isInEasternEightZones())
+        if (isInEasternEightZones()) {
             time = toDate(sdate);
-        else
-            time = TimeZoneUtil.transformTime(toDate(sdate),
-                    TimeZone.getTimeZone("GMT+08"), TimeZone.getDefault());
+        } else {
+            time = transformTime(toDate(sdate), TimeZone.getTimeZone("GMT+08"),
+                    TimeZone.getDefault());
+        }
 
         if (time == null) {
             return "Unknown";
@@ -124,437 +288,50 @@ public class StringUtils {
         return ftime;
     }
 
-    public static String friendly_time2(String sdate) {
-        String res = "";
-        if (isEmpty(sdate))
-            return "";
-
-        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
-        String currentData = StringUtils.getDataTime("MM-dd");
-        int currentDay = toInt(currentData.substring(3));
-        int currentMoth = toInt(currentData.substring(0, 2));
-
-        int sMoth = toInt(sdate.substring(5, 7));
-        int sDay = toInt(sdate.substring(8, 10));
-        int sYear = toInt(sdate.substring(0, 4));
-        Date dt = new Date(sYear, sMoth - 1, sDay - 1);
-
-        if (sDay == currentDay && sMoth == currentMoth) {
-            res = "今天 / " + weekDays[getWeekOfDate(new Date())];
-        } else if (sDay == currentDay + 1 && sMoth == currentMoth) {
-            res = "昨天 / " + weekDays[(getWeekOfDate(new Date()) + 6) % 7];
-        } else {
-            if (sMoth < 10) {
-                res = "0";
-            }
-            res += sMoth + "/";
-            if (sDay < 10) {
-                res += "0";
-            }
-            res += sDay + " / " + weekDays[getWeekOfDate(dt)];
-        }
-
-        return res;
-    }
-
-
     /**
-     * 智能格式化
-     */
-    public static String friendly_time3(String sdate) {
-        String res = "";
-        if (isEmpty(sdate))
-            return "";
-
-        Date date = StringUtils.toDate(sdate);
-        if (date == null)
-            return sdate;
-
-        SimpleDateFormat format = dateFormater2.get();
-
-        if (isToday(date.getTime())) {
-            format.applyPattern(isMorning(date.getTime()) ? "上午 hh:mm" : "下午 hh:mm");
-            res = format.format(date);
-        } else if (isYesterday(date.getTime())) {
-            format.applyPattern(isMorning(date.getTime()) ? "昨天 上午 hh:mm" : "昨天 下午 hh:mm");
-            res = format.format(date);
-        } else if (isCurrentYear(date.getTime())) {
-            format.applyPattern(isMorning(date.getTime()) ? "MM-dd 上午 hh:mm" : "MM-dd 下午 hh:mm");
-            res = format.format(date);
-        } else {
-            format.applyPattern(isMorning(date.getTime()) ? "yyyy-MM-dd 上午 hh:mm" : "yyyy-MM-dd 下午 hh:mm");
-            res = format.format(date);
-        }
-        return res;
-    }
-
-    /**
-     * @return 判断一个时间是不是上午
-     */
-    public static boolean isMorning(long when) {
-        android.text.format.Time time = new android.text.format.Time();
-        time.set(when);
-
-        int hour = time.hour;
-        return (hour >= 0) && (hour < 12);
-    }
-
-    /**
-     * @return 判断一个时间是不是今天
-     */
-    public static boolean isToday(long when) {
-        android.text.format.Time time = new android.text.format.Time();
-        time.set(when);
-
-        int thenYear = time.year;
-        int thenMonth = time.month;
-        int thenMonthDay = time.monthDay;
-
-        time.set(System.currentTimeMillis());
-        return (thenYear == time.year)
-                && (thenMonth == time.month)
-                && (thenMonthDay == time.monthDay);
-    }
-
-    /**
-     * @return 判断一个时间是不是昨天
-     */
-    public static boolean isYesterday(long when) {
-        android.text.format.Time time = new android.text.format.Time();
-        time.set(when);
-
-        int thenYear = time.year;
-        int thenMonth = time.month;
-        int thenMonthDay = time.monthDay;
-
-        time.set(System.currentTimeMillis());
-        return (thenYear == time.year)
-                && (thenMonth == time.month)
-                && (time.monthDay - thenMonthDay == 1);
-    }
-
-    /**
-     * @return 判断一个时间是不是今年
-     */
-    public static boolean isCurrentYear(long when) {
-        android.text.format.Time time = new android.text.format.Time();
-        time.set(when);
-
-        int thenYear = time.year;
-
-        time.set(System.currentTimeMillis());
-        return (thenYear == time.year);
-    }
-
-    /**
-     * 获取当前日期是星期几<br>
-     *
-     * @param dt
-     * @return 当前日期是星期几
-     */
-    public static int getWeekOfDate(Date dt) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(dt);
-        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
-        if (w < 0)
-            w = 0;
-        return w;
-    }
-
-    /**
-     * 判断给定字符串时间是否为今日
+     * 将字符串转位日期类型
      *
      * @param sdate
-     * @return boolean
-     */
-    public static boolean isToday(String sdate) {
-        boolean b = false;
-        Date time = toDate(sdate);
-        Date today = new Date();
-        if (time != null) {
-            String nowDate = dateFormater2.get().format(today);
-            String timeDate = dateFormater2.get().format(time);
-            if (nowDate.equals(timeDate)) {
-                b = true;
-            }
-        }
-        return b;
-    }
-
-    /**
-     * 返回long类型的今天的日期
-     *
      * @return
      */
-    public static long getToday() {
-        Calendar cal = Calendar.getInstance();
-        String curDate = dateFormater2.get().format(cal.getTime());
-        curDate = curDate.replace("-", "");
-        return Long.parseLong(curDate);
+    public static Date toDate(String sdate) {
+        return toDate(sdate, dateFormater.get());
     }
 
-    public static String getCurTimeStr() {
-        Calendar cal = Calendar.getInstance();
-        String curDate = dateFormater.get().format(cal.getTime());
-        return curDate;
-    }
-
-    /***
-     * 计算两个时间差，返回的是的秒s
-     *
-     * @param dete1
-     * @param date2
-     * @return
-     * @author 火蚁 2015-2-9 下午4:50:06
-     */
-    public static long calDateDifferent(String dete1, String date2) {
-
-        long diff = 0;
-
-        Date d1 = null;
-        Date d2 = null;
-
+    public static Date toDate(String sdate, SimpleDateFormat dateFormater) {
         try {
-            d1 = dateFormater.get().parse(dete1);
-            d2 = dateFormater.get().parse(date2);
-
-            // 毫秒ms
-            diff = d2.getTime() - d1.getTime();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            return dateFormater.parse(sdate);
+        } catch (ParseException e) {
+            return null;
         }
-
-        return diff / 1000;
     }
 
     /**
-     * 判断给定字符串是否空白串。 空白串是指由空格、制表符、回车符、换行符组成的字符串 若输入字符串为null或空字符串，返回true
-     *
-     * @param input
-     * @return boolean
-     */
-    public static boolean isEmpty(String input) {
-        if (input == null || "".equals(input))
-            return true;
-
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (c != ' ' && c != '\t' && c != '\r' && c != '\n') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 判断是不是一个合法的电子邮件地址
-     *
-     * @param email
-     * @return
-     */
-    public static boolean isEmail(String email) {
-        if (email == null || email.trim().length() == 0)
-            return false;
-        return emailer.matcher(email).matches();
-    }
-
-    /**
-     * 判断一个url是否为图片url
-     *
-     * @param url
-     * @return
-     */
-    public static boolean isImgUrl(String url) {
-        if (url == null || url.trim().length() == 0)
-            return false;
-        return IMG_URL.matcher(url).matches();
-    }
-
-    /**
-     * 判断是否为一个合法的url地址
-     *
-     * @param str
-     * @return
-     */
-    public static boolean isUrl(String str) {
-        if (str == null || str.trim().length() == 0)
-            return false;
-        return URL.matcher(str).matches();
-    }
-
-    /**
-     * 字符串转整数
-     *
-     * @param str
-     * @param defValue
-     * @return
-     */
-    public static int toInt(String str, int defValue) {
-        try {
-            return Integer.parseInt(str);
-        } catch (Exception e) {
-        }
-        return defValue;
-    }
-
-    /**
-     * 对象转整数
-     *
-     * @param obj
-     * @return 转换异常返回 0
-     */
-    public static int toInt(Object obj) {
-        if (obj == null)
-            return 0;
-        return toInt(obj.toString(), 0);
-    }
-
-    /**
-     * 对象转整数
-     *
-     * @param obj
-     * @return 转换异常返回 0
-     */
-    public static long toLong(String obj) {
-        try {
-            return Long.parseLong(obj);
-        } catch (Exception e) {
-        }
-        return 0;
-    }
-
-    /**
-     * 字符串转布尔值
-     *
-     * @param b
-     * @return 转换异常返回 false
-     */
-    public static boolean toBool(String b) {
-        try {
-            return Boolean.parseBoolean(b);
-        } catch (Exception e) {
-        }
-        return false;
-    }
-
-    public static String getString(String s) {
-        return s == null ? "" : s;
-    }
-
-    /**
-     * 将一个InputStream流转换成字符串
-     *
-     * @param is
-     * @return
-     */
-    public static String toConvertString(InputStream is) {
-        StringBuffer res = new StringBuffer();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader read = new BufferedReader(isr);
-        try {
-            String line;
-            line = read.readLine();
-            while (line != null) {
-                res.append(line + "<br>");
-                line = read.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (null != isr) {
-                    isr.close();
-                    isr.close();
-                }
-                if (null != read) {
-                    read.close();
-                    read = null;
-                }
-                if (null != is) {
-                    is.close();
-                    is = null;
-                }
-            } catch (IOException e) {
-            }
-        }
-        return res.toString();
-    }
-
-    /***
-     * 截取字符串
-     *
-     * @param start 从那里开始，0算起
-     * @param num   截取多少个
-     * @param str   截取的字符串
-     * @return
-     */
-    public static String getSubString(int start, int num, String str) {
-        if (str == null) {
-            return "";
-        }
-        int leng = str.length();
-        if (start < 0) {
-            start = 0;
-        }
-        if (start > leng) {
-            start = leng;
-        }
-        if (num < 0) {
-            num = 1;
-        }
-        int end = start + num;
-        if (end > leng) {
-            end = leng;
-        }
-        return str.substring(start, end);
-    }
-
-    /**
-     * 获取当前时间为每年第几周
+     * 判断用户的设备时区是否为东八区（中国） 2014年7月31日
      *
      * @return
      */
-    public static int getWeekOfYear() {
-        return getWeekOfYear(new Date());
+    public static boolean isInEasternEightZones() {
+        boolean defaultVaule = true;
+        if (TimeZone.getDefault() == TimeZone.getTimeZone("GMT+08"))
+            defaultVaule = true;
+        else
+            defaultVaule = false;
+        return defaultVaule;
     }
 
     /**
-     * 获取当前时间为每年第几周
-     *
-     * @param date
-     * @return
+     * 根据不同时区，转换时间 2014年7月31日
      */
-    public static int getWeekOfYear(Date date) {
-        Calendar c = Calendar.getInstance();
-        c.setFirstDayOfWeek(Calendar.MONDAY);
-        c.setTime(date);
-        int week = c.get(Calendar.WEEK_OF_YEAR) - 1;
-        week = week == 0 ? 52 : week;
-        return week > 0 ? week : 1;
-    }
-
-    public static int[] getCurrentDate() {
-        int[] dateBundle = new int[3];
-        String[] temp = getDataTime("yyyy-MM-dd").split("-");
-
-        for (int i = 0; i < 3; i++) {
-            try {
-                dateBundle[i] = Integer.parseInt(temp[i]);
-            } catch (Exception e) {
-                dateBundle[i] = 0;
-            }
+    public static Date transformTime(Date date, TimeZone oldZone,
+                                     TimeZone newZone) {
+        Date finalDate = null;
+        if (date != null) {
+            int timeOffset = oldZone.getOffset(date.getTime())
+                    - newZone.getOffset(date.getTime());
+            finalDate = new Date(date.getTime() - timeOffset);
         }
-        return dateBundle;
+        return finalDate;
     }
-
-    /**
-     * 返回当前系统时间
-     */
-    public static String getDataTime(String format) {
-        SimpleDateFormat df = new SimpleDateFormat(format);
-        return df.format(new Date());
-    }
-
 
 }
