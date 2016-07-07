62d6441a5ab9fc88e49e6572d1917e6835d2dfff
安诺爱成长 <1399487511@qq.com>
2016-07-07 17:28:46 +0800

    增加ReplaceActivity

---
+++ app/src/main/java/com/ht/fyforandroid/base/ReplaceActivity.java
@@ -0,0 +1,48 @@
+package com.ht.fyforandroid.base;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+
+import com.ht.fyforandroid.R;
+
+/**
+ * Created by niehongtao on 16/7/7.
+ */
+public class ReplaceActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_replace;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+
+    public void replaceFragment(Fragment fragment) {
+        replaceFragment(fragment, false);
+    }
+
+    public void replaceFragment(Fragment fragment, boolean addToBackStack) {
+        if (fragment == null || fragment == getSupportFragmentManager().findFragmentById(R.id.container))
+            return;
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+}
