fe2d916b9d9ba29cf9adbb19b375b424cbbb11c8
安诺爱成长 <1399487511@qq.com>
2016-07-20 20:33:44 +0800

    增加扩展性

--- app/src/main/java/com/ht/fyforandroid/test/mvptest/presenter/WeatherPresenter.java
+++ app/src/main/java/com/ht/fyforandroid/test/mvptest/presenter/WeatherPresenter.java
@@ -1,19 +1,20 @@
 package com.ht.fyforandroid.test.mvptest.presenter;
 
+import com.ht.fyforandroid.base.BaseModel;
 import com.ht.fyforandroid.base.BasePresenter;
 import com.ht.fyforandroid.net.base.BaseRequest;
 import com.ht.fyforandroid.net.base.BaseResponse;
 import com.ht.fyforandroid.net.volleynet.VolleyNetHelper;
 import com.ht.fyforandroid.test.mvptest.bean.WeatherEntity;
-import com.ht.fyforandroid.test.mvptest.model.WeatherMedel;
 import com.ht.fyforandroid.test.mvptest.view.WeatherView;
 
 import java.util.Map;
 
 /**
  * Created by niehongtao on 16/7/18.
+ * 如果没有Model，第二个参数写成BaseModel即可
  */
-public class WeatherPresenter extends BasePresenter<WeatherView, WeatherMedel> {
+public class WeatherPresenter extends BasePresenter<WeatherView, BaseModel> {
 
     public void fetchList() {
         mView.onShowLoading();
