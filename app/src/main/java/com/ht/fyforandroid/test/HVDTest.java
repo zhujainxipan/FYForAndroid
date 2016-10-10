04113d72908de00fb45d6805b670c8e59745088e
安诺爱成长 <1399487511@qq.com>
2016-10-10 14:27:41 +0800

    测试显示隐藏

---
+++ app/src/main/java/com/ht/fyforandroid/test/HVDTest.java
@@ -0,0 +1,21 @@
+package com.ht.fyforandroid.test;
+
+import android.os.Bundle;
+
+import com.ht.fyforandroid.R;
+import com.ht.fyforandroid.base.BaseActivity;
+
+/**
+ * Created by niehongtao on 16/10/10.
+ */
+public class HVDTest extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_hvd;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+}
